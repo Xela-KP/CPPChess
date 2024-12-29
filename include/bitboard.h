9e01800bd561e0038e8bc5580b1ba91b2b7d40d9
Alexander Perocho <perocho.xela@gmail.com>
2024-12-28 21:45:59 -0500

    mapped knight and king moves

--- include/bitboard.h
+++ include/bitboard.h
@@ -5,11 +5,13 @@
 #define set_bit(bitboard, square) (bitboard |= (1ULL << square))
 #define clear_bit(bitborad, square) (bitboard &= ~(1ULL << square))
 
-U64 pawn_attacks[2][BOARD_SIZE];
 const U64 NOT_A_FILE = 18374403900871474942ULL;
 const U64 NOT_H_FILE = 9187201950435737471ULL;
 const U64 NOT_HG_FILE = 4557430888798830399ULL;
 const U64 NOT_AB_FILE = 18229723555195321596ULL;
+U64 pawn_attacks[2][BOARD_SIZE];
+U64 knight_attacks[BOARD_SIZE];
+U64 king_attacks[BOARD_SIZE];
 
 void print_bitboard(U64 bitboard)
 {
@@ -49,11 +51,63 @@ U64 mask_pawn_attacks(int side, int from_square)
     return attack_bitboard;
 }
 
+U64 mask_knight_attacks(int from_square)
+{
+    U64 bitboard = 0ULL;
+    U64 attack_bitboard = 0ULL;
+    set_bit(bitboard, from_square);
+    if (bitboard & NOT_A_FILE)
+    {
+        attack_bitboard |= bitboard >> 17;
+        attack_bitboard |= bitboard << 15;
+    }
+    if (bitboard & NOT_H_FILE)
+    {
+        attack_bitboard |= bitboard << 17;
+        attack_bitboard |= bitboard >> 15;
+    }
+    if (bitboard & NOT_AB_FILE)
+    {
+        attack_bitboard |= bitboard >> 10;
+        attack_bitboard |= bitboard << 6;
+    }
+    if (bitboard & NOT_HG_FILE)
+    {
+        attack_bitboard |= bitboard << 10;
+        attack_bitboard |= bitboard >> 6;
+    }
+    return attack_bitboard;
+}
+
+U64 mask_king_attacks(int from_square)
+{
+    U64 bitboard = 0ULL;
+    U64 attack_bitboard = 0ULL;
+    set_bit(bitboard, from_square);
+    attack_bitboard |= bitboard >> 8;
+    attack_bitboard |= bitboard << 8;
+    if (bitboard & NOT_A_FILE)
+    {
+        attack_bitboard |= bitboard >> 1;
+        attack_bitboard |= bitboard >> 9;
+        attack_bitboard |= bitboard << 7;
+    }
+    if (bitboard & NOT_H_FILE)
+    {
+        attack_bitboard |= bitboard << 1;
+        attack_bitboard |= bitboard << 9;
+        attack_bitboard |= bitboard >> 7;
+    }
+    return attack_bitboard;
+}
+
 void map_leap_attacks()
 {
-    for (int from_square = a8; from_square != h1; from_square++)
+    for (int from_square = a8; from_square <= h1; from_square++)
     {
         pawn_attacks[white][from_square] = mask_pawn_attacks(white, from_square);
         pawn_attacks[black][from_square] = mask_pawn_attacks(black, from_square);
+        knight_attacks[from_square] = mask_knight_attacks(from_square);
+        king_attacks[from_square] = mask_king_attacks(from_square);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include "../include/bitboard.h"
 int main()
 {
-    U64 bitboard = 0ULL;
+    U64 bitboard = 1ULL;
     map_leap_attacks();
-    print_bitboard(pawn_attacks[white][a2]);
+    print_bitboard(knight_attacks[a8]);
     return 0;
 }
