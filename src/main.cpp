cdfede804ef711d37403324cac9735264fb81fd2
Alexander Perocho <perocho.xela@gmail.com>
2024-12-31 04:51:49 -0500

    test get_num_attackers_on

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 #include "../include/fen.cpp"
 int main()
 {
-    U64 bitboard = 0ULL;
     mask::map_slide_attacks();
-    mask::initialize_occupancies();
-    bitboard::set_bit(bitboard, chess::d5);
-    bitboard::set_bit(bitboard, chess::d3);
-    util::print_bitboard(mask::get_queen_attack_mask(chess::d4, bitboard));
-    // util::print_bitboard(mask::queen_attack_mask[]);
-    // fen::parse_fen(KILLER_POSITION);
-    // util::print_chessboard();
+    mask::map_leap_attacks();
+
+    fen::parse_fen(TRICKY_POSITION);
+    // bitboard::clear_bit(mask::piece_occupancies[chess::r], chess::h8);
+    // bitboard::clear_bit(mask::piece_occupancies[chess::n], chess::f6);
+    // bitboard::clear_bit(mask::piece_occupancies[chess::p], chess::g6);
+    util::print_chessboard();
+    std::cout << (mask::get_attackers_on(chess::BLACK, chess::d5));
     return 0;
 }
