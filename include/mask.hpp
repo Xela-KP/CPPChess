ad0b8f690fbf74818c78069459cf8cad6f905099
Alexander Perocho <perocho.xela@gmail.com>
2024-12-31 05:05:42 -0500

    refactored

--- include/mask.hpp
+++ include/mask.hpp
@@ -3,6 +3,18 @@
 #include "bitboard.hpp"
 namespace mask
 {
+    const U64 NOT_A_FILE = 18374403900871474942ULL;
+    const U64 NOT_H_FILE = 9187201950435737471ULL;
+    const U64 NOT_HG_FILE = 4557430888798830399ULL;
+    const U64 NOT_AB_FILE = 18229723555195321596ULL;
+    U64 pawn_attack_mask[2][BOARD_SIZE];
+    U64 knight_attack_mask[BOARD_SIZE];
+    U64 king_attack_mask[BOARD_SIZE];
+    U64 bishop_unblocked_attack_mask[BOARD_SIZE];
+    U64 rook_unblocked_attack_mask[BOARD_SIZE];
+    U64 bishop_attack_mask[BOARD_SIZE][4096];
+    U64 rook_attack_mask[BOARD_SIZE][4096];
+    U64 queen_attack_mask[BOARD_SIZE][4096];
     U64 get_masked_occupancy(int occupied, U64 attack_mask);
     U64 get_pawn_attack_mask(int side, int from_square);
     U64 get_knight_attack_mask(int from_square);
@@ -16,16 +28,4 @@ namespace mask
     static inline int get_num_attackers_on(int side, int square);
     void map_leap_attacks();
     void map_slide_attacks();
-    const U64 NOT_A_FILE = 18374403900871474942ULL;
-    const U64 NOT_H_FILE = 9187201950435737471ULL;
-    const U64 NOT_HG_FILE = 4557430888798830399ULL;
-    const U64 NOT_AB_FILE = 18229723555195321596ULL;
-    U64 pawn_attack_mask[2][BOARD_SIZE];
-    U64 knight_attack_mask[BOARD_SIZE];
-    U64 king_attack_mask[BOARD_SIZE];
-    U64 bishop_unblocked_attack_mask[BOARD_SIZE];
-    U64 rook_unblocked_attack_mask[BOARD_SIZE];
-    U64 bishop_attack_mask[BOARD_SIZE][4096];
-    U64 rook_attack_mask[BOARD_SIZE][4096];
-    U64 queen_attack_mask[BOARD_SIZE][4096];
 }
