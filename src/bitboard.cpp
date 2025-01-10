68d450f047e465715ac8293a09d29c5036ab7226
Alexander King Perocho <perocho.xela@gmail.com>
2025-01-10 15:50:09 -0500

    refactor

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,16 +1,14 @@
 #include "../include/bitboard.hpp"
-namespace BitboardUtils
+
+int BitboardUtils::getLSB(Bitboard bitboard) { return __builtin_ctzll(bitboard); }
+int BitboardUtils::getBitCount(Bitboard bitboard) { return __builtin_popcountll(bitboard); }
+int BitboardUtils::getSquare(int rank, int file) { return rank * 8 + file; }
+bool BitboardUtils::getBit(Bitboard bitboard, int square) { return (bitboard & 1ULL << (square)) != 0; }
+void BitboardUtils::setBit(Bitboard &bitboard, int square) { bitboard |= (1ULL << square); }
+void BitboardUtils::clearBit(Bitboard &bitboard, int square) { bitboard &= ~(1ULL << square); }
+int BitboardUtils::popLSB(Bitboard &bitboard)
 {
-    int getLSB(Bitboard bitboard) { return __builtin_ctzll(bitboard); }
-    int getBitCount(Bitboard bitboard) { return __builtin_popcountll(bitboard); }
-    int getSquare(int rank, int file) { return rank * 8 + file; }
-    bool getBit(Bitboard bitboard, int square) { return (bitboard & 1ULL << (square)) != 0; }
-    void setBit(Bitboard &bitboard, int square) { bitboard |= (1ULL << square); }
-    void clearBit(Bitboard &bitboard, int square) { bitboard &= ~(1ULL << square); }
-    int popLSB(Bitboard &bitboard)
-    {
-        int i = getLSB(bitboard);
-        bitboard &= bitboard - 1;
-        return i == 64 ? -1 : i;
-    }
+    int i = getLSB(bitboard);
+    bitboard &= bitboard - 1;
+    return i == 64 ? -1 : i;
 }
