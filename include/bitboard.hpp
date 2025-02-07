6eebb6061bc0860e0ab7883435b085a812845eb0
Alexander Perocho <perocho.xela@gmail.com>
2025-02-07 15:21:54 -0500

    refactored bitboard

--- include/bitboard.hpp
+++ include/bitboard.hpp
@@ -1,17 +1,63 @@
 #pragma once
+#include <bitset>
 #include <cstdint>
-typedef unsigned long long Bitboard;
-constexpr int BOARD_SIZE = 64;
-constexpr int DIMENSION = 8;
-constexpr Bitboard EMPTY_BOARD = 0ULL;
-constexpr Bitboard FULL_BOARD = ~EMPTY_BOARD;
-namespace BitboardUtils
+// typedef unsigned long long Bitboard;
+// constexpr int BOARD_SIZE = 64;
+// constexpr int DIMENSION = 8;
+// constexpr Bitboard EMPTY_BOARD = 0ULL;
+// constexpr Bitboard FULL_BOARD = ~EMPTY_BOARD;
+// namespace BitboardUtils
+// {
+//     int getLSB(Bitboard bitboard);
+//     int getBitCount(Bitboard bitboard);
+//     int getSquare(int rank, int file);
+//     int popLSB(Bitboard &bitboard);
+//     bool getBit(Bitboard bitboard, int square);
+//     void setBit(Bitboard &bitboard, int square);
+//     void clearBit(Bitboard &bitboard, int square);
+// }
+
+class Bitboard
 {
-    int getLSB(Bitboard bitboard);
-    int getBitCount(Bitboard bitboard);
-    int getSquare(int rank, int file);
-    int popLSB(Bitboard &bitboard);
-    bool getBit(Bitboard bitboard, int square);
-    void setBit(Bitboard &bitboard, int square);
-    void clearBit(Bitboard &bitboard, int square);
-}
+public:
+    Bitboard();
+    Bitboard(uint64_t value);
+    inline operator uint64_t() const;
+
+    class BitProxy;
+
+    inline bool operator[](int square) const;
+    inline BitProxy operator[](int square);
+
+    inline Bitboard operator&(const Bitboard &other) const;
+    inline Bitboard operator|(const Bitboard &other) const;
+    inline Bitboard operator^(const Bitboard &other) const;
+    inline Bitboard operator~() const;
+    inline Bitboard operator<<(int shift) const;
+    inline Bitboard operator>>(int shift) const;
+
+    inline Bitboard &operator&=(const Bitboard &other);
+    inline Bitboard &operator|=(const Bitboard &other);
+    inline Bitboard &operator^=(const Bitboard &other);
+    inline Bitboard &operator<<=(int shift);
+    inline Bitboard &operator>>=(int shift);
+
+    inline int GetLsbIndex() const;
+    inline int PopLsbIndex();
+
+    void print() const;
+
+    class BitProxy
+    {
+    public:
+        BitProxy(Bitboard &bb, int sq);
+        BitProxy &operator=(bool value);
+        operator bool() const;
+    private:
+        Bitboard &bitboard;
+        int square;
+    };
+
+private:
+    uint64_t value;
+};
--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,14 +1,98 @@
 #include "../include/bitboard.hpp"
+#include <iostream>
 
-int BitboardUtils::getLSB(Bitboard bitboard) { return __builtin_ctzll(bitboard); }
-int BitboardUtils::getBitCount(Bitboard bitboard) { return __builtin_popcountll(bitboard); }
-int BitboardUtils::getSquare(int rank, int file) { return rank * 8 + file; }
-bool BitboardUtils::getBit(Bitboard bitboard, int square) { return (bitboard & 1ULL << (square)) != 0; }
-void BitboardUtils::setBit(Bitboard &bitboard, int square) { bitboard |= (1ULL << square); }
-void BitboardUtils::clearBit(Bitboard &bitboard, int square) { bitboard &= ~(1ULL << square); }
-int BitboardUtils::popLSB(Bitboard &bitboard)
+// int BitboardUtils::getLSB(Bitboard bitboard) { return __builtin_ctzll(bitboard); }
+// int BitboardUtils::getBitCount(Bitboard bitboard) { return __builtin_popcountll(bitboard); }
+// int BitboardUtils::getSquare(int rank, int file) { return rank * 8 + file; }
+// bool BitboardUtils::getBit(Bitboard bitboard, int square) { return (bitboard & 1ULL << (square)) != 0; }
+// void BitboardUtils::setBit(Bitboard &bitboard, int square) { bitboard |= (1ULL << square); }
+// void BitboardUtils::clearBit(Bitboard &bitboard, int square) { bitboard &= ~(1ULL << square); }
+// int BitboardUtils::popLSB(Bitboard &bitboard)
+// {
+//     int i = getLSB(bitboard);
+//     bitboard &= bitboard - 1;
+//     return i == 64 ? -1 : i;
+// }
+
+
+
+Bitboard::Bitboard() : value(0) {}
+Bitboard::Bitboard(uint64_t value) : value(value) {}
+Bitboard::operator uint64_t() const { return value; }
+
+bool Bitboard::operator[](int square) const { return (value & (1ULL << square)) != 0; }
+Bitboard::BitProxy::BitProxy(Bitboard &bitbaord, int square) : bitboard(bitboard), square(square) {}
+Bitboard::BitProxy &Bitboard::BitProxy::operator=(bool value)
 {
-    int i = getLSB(bitboard);
-    bitboard &= bitboard - 1;
-    return i == 64 ? -1 : i;
+    value ? bitboard.value |= (1ULL << square) : bitboard.value &= ~(1ULL << square);
+    return *this;
+}
+
+ // (bitboard.value & (1ULL << square)) != 0;
+Bitboard::BitProxy::operator bool() const { return (bitboard.value & (1ULL << square)); }
+Bitboard::BitProxy Bitboard::operator[](int square) { return BitProxy(*this, square); }
+
+// Bitwise operators
+Bitboard Bitboard::operator&(const Bitboard &other) const { return Bitboard(value & other.value); }
+Bitboard Bitboard::operator|(const Bitboard &other) const { return Bitboard(value | other.value); }
+Bitboard Bitboard::operator^(const Bitboard &other) const { return Bitboard(value ^ other.value); }
+Bitboard Bitboard::operator~() const { return Bitboard(~value); }
+Bitboard Bitboard::operator<<(int shift) const { return Bitboard(value << shift); }
+Bitboard Bitboard::operator>>(int shift) const { return Bitboard(value >> shift); }
+
+// Compound assignment operators
+Bitboard &Bitboard::operator&=(const Bitboard &other)
+{
+    value &= other.value;
+    return *this;
+}
+
+Bitboard &Bitboard::operator|=(const Bitboard &other)
+{
+    value |= other.value;
+    return *this;
+}
+
+Bitboard &Bitboard::operator^=(const Bitboard &other)
+{
+    value ^= other.value;
+    return *this;
+}
+
+Bitboard &Bitboard::operator<<=(int shift)
+{
+    value <<= shift;
+    return *this;
+}
+
+Bitboard &Bitboard::operator>>=(int shift)
+{
+    value >>= shift;
+    return *this;
+}
+
+int Bitboard::GetLsbIndex() const { return __builtin_ctzll(value); }
+
+int Bitboard::PopLsbIndex()
+{
+    if (value <= 0)
+        return -1;
+    int index = GetLsbIndex();
+    value &= value - 1;
+    return (index >= 64) ? -1 : index; 
+}
+
+
+void Bitboard::print() const
+{
+    std::bitset<64> bits(value);
+    for (int rank = 7; rank >= 0; --rank)
+    {
+        for (int file = 0; file < 8; ++file)
+        {
+            int square = rank * 8 + file;
+            std::cout << (*this)[square] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
