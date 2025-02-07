bca6cfc780c219fe8b98f09a78f0740ffbade089
Alexander Perocho <perocho.xela@gmail.com>
2025-02-07 17:16:49 -0500

    refactored move

--- include/move.hpp
+++ include/move.hpp
@@ -10,14 +10,22 @@ public:
     int Encode(int source_square, int target_square, int piece,
                int promotion_piece, int is_capture,
                int is_double_pawn_push, int is_enpassant, int is_castle);
-    inline int DecodeTargetSquare() const;
-    inline int DecodeSourceSquare() const;
-    inline int DecodePiece() const;
-    inline int DecodePromotionPiece() const ;
-    inline int DecodeCaptureFlag() const;
-    inline int DecodeDoublePawnPushFlag() const;
-    inline int DecodeEnpassantFlag() const;
-    inline int DecodeCastleFlag() const;
-private:
+
+    enum Encoding
+    {
+        SourceSquare,
+        TargetSquare,
+        Piece,
+        PromotionPiece,
+        CaptureFlag,
+        DoublePawnPushFlag,
+        EnpassantFlag,
+        CastleFlag
+
+    };
+    int Decode(Encoding encoding) const;
+
+private: 
     int encoding;
+    int decodings[8];
 };
--- src/move.cpp
+++ src/move.cpp
@@ -6,6 +6,7 @@ Move::Move(int source_square, int target_square, int piece,
 {
     Encode(source_square, target_square, piece, promotion_piece, is_capture, is_double_pawn_push, is_enpassant, is_castle);
 }
+Move::operator int() const { return encoding; }
 int Move::Encode(int source_square, int target_square, int piece,
              int promotion_piece, int is_capture,
              int is_double_pawn_push, int is_enpassant, int is_castle)
@@ -18,14 +19,15 @@ int Move::Encode(int source_square, int target_square, int piece,
                 is_double_pawn_push << 21 |
                 is_enpassant << 22 |
                 is_castle << 23);
+
+    decodings[SourceSquare] = encoding & 0x3f;
+    decodings[TargetSquare] = (encoding & 0xfc0) >> 6;
+    decodings[Piece] = (encoding & 0xf000) >> 12;
+    decodings[PromotionPiece] = (encoding & 0xf0000) >> 16;
+    decodings[CaptureFlag] = encoding & 0x100000;
+    decodings[DoublePawnPushFlag] = encoding & 0x200000;
+    decodings[EnpassantFlag] = encoding & 0x400000;
+    decodings[CastleFlag] = encoding & 0x800000;
     return encoding;
 }
-int Move::DecodeTargetSquare() const { return (encoding & 0xfc0) >> 6; }
-int Move::DecodeSourceSquare() const { return encoding & 0x3f; }
-int Move::DecodePiece() const { return (encoding & 0xf000) >> 12; }
-int Move::DecodePromotionPiece() const { return (encoding & 0xf0000) >> 16; }
-int Move::DecodeCaptureFlag() const { return encoding & 0x100000; }
-int Move::DecodeDoublePawnPushFlag() const { return encoding & 0x200000; }
-int Move::DecodeEnpassantFlag() const { return encoding & 0x400000; }
-int Move::DecodeCastleFlag() const { return encoding & 0x800000; }
-Move::operator int() const { return encoding; }
+int Move::Decode(Encoding encoding) const { return decodings[encoding]; };
